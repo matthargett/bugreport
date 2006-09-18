ac1f47778aed0d475b186c8287df4556ce8a57fc
Doug <dcoker@users.noreply.github.com>
2006-09-18 02:35:29 +0000

    moving RegisterCollection out of X86emulator and into its own file.

--- X86emulator.cs
+++ X86emulator.cs
@@ -48,39 +48,6 @@ namespace bugreport
 		public InvalidSIBException(String message) : base(message) {}
 	}
 
-	public enum RegisterName : int {EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI};
-	
-	public class RegisterCollection
-	{
-		private AbstractValue[] registers;
-		
-		public RegisterCollection()
-		{
-			registers = new AbstractValue[8];
-			for (UInt32 i = 0; i < registers.Length; ++i) {
-				registers[i] = new AbstractValue();
-			}
-		}
-		
-		public AbstractValue this[RegisterName index]
-		{
-			get { return registers[(Int32)index]; }
-			set { registers[(Int32)index] = value; }
-		}
-		
-		public override string ToString()
-		{
-			String result = String.Empty;
-			for(UInt32 i = 0; i < registers.Length; ++i)
-			{
-				AbstractValue value = registers[i];
-				result += " " + RegisterName.GetName(typeof(RegisterName), i) + "=" + value;
-				
-			}
-			return result;
-		}
-	}
-
 	public class X86emulator
 	{
 		private UInt32 instructionPointer;
