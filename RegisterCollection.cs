440abd4e996014b80fb648af839660b7c1ecaee7
Doug <dcoker@users.noreply.github.com>
2006-09-19 03:10:56 +0000

    Add RegisterCollection.  These were missing from previous commits.

---
+++ RegisterCollection.cs
@@ -0,0 +1,38 @@
+
+
+using System;
+
+namespace bugreport
+{	
+	public enum RegisterName : int {EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI};
+	
+	public class RegisterCollection
+	{
+		private AbstractValue[] registers;
+		
+		public RegisterCollection()
+		{
+			registers = new AbstractValue[8];
+			for (UInt32 i = 0; i < registers.Length; ++i) {
+//				registers[i] = new AbstractValue();
+			}
+		}
+		
+		public AbstractValue this[RegisterName index]
+		{
+			get { return registers[(Int32)index]; }
+			set { registers[(Int32)index] = value; }
+		}
+		
+		public override string ToString()
+		{
+			String result = String.Empty;
+			for(UInt32 i = 0; i < registers.Length; ++i)
+			{
+				AbstractValue value = registers[i];
+				result += " " + RegisterName.GetName(typeof(RegisterName), i) + "=" + value;				
+			}
+			return result;
+		}
+	}
+}
