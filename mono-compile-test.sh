55ff0a9c74bf07cc7848dea439346d761ed0addd
matt_hargett <matt_hargett@21a7b954-dc19-0410-a052-b66b00eebe95>
2006-12-17 07:10:34 +0000

    Update mono compile/test script to reference nunit.mocks.

--- mono-compile-test.sh
+++ mono-compile-test.sh
@@ -1,1 +1,1 @@
-gmcs -debug -t:exe -out:bin/Debug/bugreport.exe -r:lib/nunit/nunit.framework.dll *.cs && mono --debug lib/nunit/nunit-console.exe bin/Debug/bugreport.exe
+gmcs -debug -t:exe -out:bin/Debug/bugreport.exe -r:lib/nunit/nunit.framework.dll -r:lib/nunit/nunit.mocks.dll *.cs && mono --debug lib/nunit/nunit-console.exe bin/Debug/bugreport.exe
