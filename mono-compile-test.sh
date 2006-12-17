3196f59147aad21ed970958f9a71e3365cafab56
matt_hargett <matt_hargett@21a7b954-dc19-0410-a052-b66b00eebe95>
2006-12-17 21:09:59 +0000

    Fix Gendarme code analysis warnings.

--- mono-compile-test.sh
+++ mono-compile-test.sh
@@ -1,1 +1,1 @@
-gmcs -debug -t:exe -out:bin/Debug/bugreport.exe -r:lib/nunit/nunit.framework.dll -r:lib/nunit/nunit.mocks.dll *.cs && mono --debug lib/nunit/nunit-console.exe bin/Debug/bugreport.exe
+gmcs -debug -t:exe -out:bin/Debug/bugreport.exe -r:lib/nunit/nunit.framework.dll -r:lib/nunit/nunit.mocks.dll *.cs && cp lib/nunit/nunit.framework.dll lib/nunit/nunit.mocks.dll bin/Debug && mono --debug -O=-inline lib/nunit/nunit-console.exe bin/Debug/bugreport.exe
