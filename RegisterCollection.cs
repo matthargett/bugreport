b6cb298505814ceb95a3c3261b5989efbf4a6f94
omin0us <omin0us@21a7b954-dc19-0410-a052-b66b00eebe95>
2007-01-18 10:59:01 +0000

    More FxCop warning fixes

--- RegisterCollection.cs
+++ RegisterCollection.cs
@@ -3,6 +3,7 @@
 // See LICENSE.txt for details.
 
 using System;
+using System.Text;
 
 namespace bugreport
 {	
@@ -36,22 +37,22 @@ namespace bugreport
 		
 		public override string ToString()
 		{
-			String result = String.Empty;
+			StringBuilder result = new StringBuilder(String.Empty);
 			for(UInt32 i = 0; i < registers.Length; ++i)
 			{
 				AbstractValue value = registers[i];
-				result += RegisterName.GetName(typeof(RegisterName), i) + "=" + value + "\t";
+				result.Append(RegisterName.GetName(typeof(RegisterName), i) + "=" + value + "\t");
 				if (value.ToString().Length < 8)
 				{
-					result += "\t";
+					result.Append("\t");
 				}
 
 				if ((i + 1) % 4 == 0)
 				{
-					result += Environment.NewLine;
+					result.Append(Environment.NewLine);
 				}
 			}
-			return result;
+			return result.ToString();
 		}
 	}
 }
