9fc2b43b90efb73b3915eb7ed86fed1296342e7d
dmoniz <dmoniz@21a7b954-dc19-0410-a052-b66b00eebe95>
2007-04-26 20:17:52 +0000

    Updated copyright & current authors on all C# files, normalized line endings to CRLF.

--- CoverageExcludeAttribute.cs
+++ CoverageExcludeAttribute.cs
@@ -1,3 +1,8 @@
+// Copyright (c) 2006-2007 Luis Miras, Doug Coker, Todd Nagengast,
+// Anthony Lineberry, Dan Moniz, Bryan Siepert, Mike Seery, Cullen Bryan
+// Licensed under GPLv3 draft 3
+// See LICENSE.txt for details.
+
 using System;
 
 namespace bugreport
